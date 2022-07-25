f6fbcc0e49f0329791a2f4429732bbf1fa454bff
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2022-07-25 14:13:47 +0800

    Integrate Ziggy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+// import typeScript from '@vuejs/plugin-type-script';
 
 export default defineConfig({
     plugins: [
