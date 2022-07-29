36379acdcdd7a15f5c8601924fddeaccaba59d17
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2022-07-29 13:56:38 +0800

    Incomplete test for UpdateProjectAction

---
+++ tests/Feature/Domain/Projects/UpdateProjectsActionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Domain\Projects;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UpdateProjectsActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_member_of_project_can_update_the_details()
+    {
+        $this->markTestIncomplete();
+    }
+}
